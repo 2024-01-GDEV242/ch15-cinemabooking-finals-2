17aaa97a505b08ddf30754b6b1f201096f5265f4
Paulo Santos <124013397+PauloSantos223@users.noreply.github.com>
2024-05-05 22:06:30 -0400

    Cleaned up Customer and Seat Classes

--- Customer.java
+++ Customer.java
@@ -5,72 +5,85 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Customer
-{
-    // instance variables - replace the example below with your own
-    int id;
-    String name;
-    String surname;
-    String street;
-    int streetNumber;
-    int post;
-    String city;
+public class Customer {
+    private int id;
+    private String name;
+    private String surname;
+    private String street;
+    private int streetNumber;
+    private int post;
+    private String city;
 
-    /**
-     * Constructor for objects of class Customer
-     */
-    public Customer(int id)
-    {
-        // initialise instance variables
+    public Customer(int id) {
         this.id = id;
     }
+    
+    public Customer(int id, String name, String surname, String street, int streetNumber, int post, String city) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.street = street;
+        this.streetNumber = streetNumber;
+        this.post = post;
+        this.city = city;
+    }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int getId()
-    {
-        // put your code here
+    public int getId() {
         return id;
     }
     
-    public void setName(String name)
-    {
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
         this.name = name;
     }
     
-    public void setSurname(String surname)
-    {
+    public String getSurname() {
+        return surname;
+    }
+    
+    public void setSurname(String surname) {
         this.surname = surname;
     }
     
-    public void setStreet(String street)
-    {
+    public String getStreet() {
+        return street;
+    }
+    
+    public void setStreet(String street) {
         this.street = street;
     }
     
-    public void setStreetNumber(int streetNumber)
-    {
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+    
+    public void setStreetNumber(int streetNumber) {
         this.streetNumber = streetNumber;
     }
     
-    public void setPostal(int post)
-    {
+    public int getPost() {
+        return post;
+    }
+    
+    public void setPost(int post) {
         this.post = post;
     }
     
-    public void setCity(String city)
-    {
+    public String getCity() {
+        return city;
+    }
+    
+    public void setCity(String city) {
         this.city = city;
     }
     
-    public void printCostumer()
-    {
-    System.out.print(surname + " " + name);
-    System.out.println(street + " " + streetNumber);
-    System.out.println(post + " " + city);
+    public void printCustomer() {
+        System.out.println(surname + " " + name);
+        System.out.println(street + " " + streetNumber);
+        System.out.println(post + " " + city);
     }
 }
+
--- Seat.java
+++ Seat.java
@@ -5,51 +5,29 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Seat
-{
-    // instance variables - replace the example below with your own
-    int seatNumber;
-    boolean isReserved;
+public class Seat {
+    private int seatNumber;
+    private boolean isReserved;
 
-    /**
-     * Constructor for objects of class Seat
-     */
-    public Seat(boolean isReserved, int seatNumber)
-    {
-        // initialise instance variables
+    public Seat(boolean isReserved, int seatNumber) {
         this.isReserved = isReserved;
         this.seatNumber = seatNumber;
     }
-    
-    public void setSeatNumber(int seatNumber)
-    {
-        this.seatNumber = seatNumber;
-    }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int getSeatNumber()
-    {
-        // put your code here
+    public int getSeatNumber() {
         return this.seatNumber;
     }
-    
-    public boolean getReservationStatus()
-    {
+
+    public boolean getReservationStatus() {
         return isReserved;
     }
-    
-    public void reserve()
-    {
+
+    public void reserve() {
         isReserved = true;
     }
-    
-    public void unreserve()
-    {
+
+    public void unreserve() {
         isReserved = false;
     }
 }
+
